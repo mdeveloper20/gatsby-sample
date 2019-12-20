7fb2fa8597467fa1265bceb3d0aeacdb00643dfe
Masoud Pezeshkzade <masoud.pezeshkzade@gmail.com>
2019-12-20 20:35:41 +0330

    add gatsby.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Header from "../components/header"
+import {Link} from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Header >Home Page</Header>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <br />
+    <Link to={'/about/'}>About</Link>
+  </div>
+)
